0beb1817f2aa92c74a7e46ed9457207fbe6d82dd
douruinan <1059617833@qq.com>
2020-11-08 16:46:21 +0800

    美化了登录页面

--- login.php
+++ login.php
@@ -1,19 +1,54 @@
-<form action="loginsuccess.php" method="post">
- <fieldset>
-  <legend>用户登录</legend>
-  <ul>
-  <li>
-   <label>用户名:</label>
-   <input type="text" name="username">
-  </li>
-  <li>
-   <label>密  码:</label>
-   <input type="password" name="password">
-  </li>
-  <li>
-   <label> </label>
-   <input type="submit" name="login" value="登录">
-  </li>
-  </ul>
- </fieldset>
- </form>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>用户登录</title>
+  <style type="text/css">
+    #foot{
+        height:30px;
+        background-color:powderblue;
+        margin: 0px 0px;
+        font-size:30;
+      }
+      #b{
+        height: 100px;
+      }
+       #5{
+        height: 50px;
+      }
+  </style>
+</head>
+<body>
+  <div id="b">
+    <p align="center"><font face="隶书" size="+4" color="#cccc00">郑州师范学院2019大创项目-组卷系统</font></p>
+      </div>
+  <table align="center" >
+    <tr align="center"><td width="80%" align="center">
+    <form action="loginsuccess.php" method="post">
+     <fieldset>
+      <legend>用户登录</legend>
+      <ul>
+      <li>
+       <label>用户名:</label>
+       <input type="text" name="username">
+      </li>
+      <li>
+       <label>密  码:</label>
+       <input type="password" name="password">
+      </li>
+      <li>
+       <label> </label>
+       <input type="submit" name="login" value="登录">
+      </li>
+      </ul>
+     </fieldset>
+  </form></td></tr></table>
+</div>
+<div id="5"><</div>
+   <div id="foot">
+    <p align="center"><font color="white">版权所有&copy;郑州师范学院</font></p>
+      </div>
+     </form>
+</body>
+</html>
+
+
