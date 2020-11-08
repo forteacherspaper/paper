0387c2def9a774e87ace430717c8bf8229fad6a0
Somuch-make <1993700090@qq.com>
2020-11-08 16:22:28 +0800

    在首页添加了用session记录课程id的功能，删除添加题目页显示sql语句的语句

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+    if(isset($_GET['ID']))
+        $_SESSION['courseid']=$_GET['ID'];
+    else
+        header("location:selectcourse.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
